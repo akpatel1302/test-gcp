7fac8e28d63e82b9b016e1353242bf8a481f06e6
Ayush patel <88823832+akpatel1302@users.noreply.github.com>
2024-10-25 10:13:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,15 +9,10 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
-  profile = "your_aws_profile" # Replace with your AWS CLI profile name
+  region  = "us-east-1"
 }
 
 resource "aws_instance" "example_server" {
   ami           = "ami-04e914639d0cca79a" # Amazon Linux 2 AMI
   instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleServer"
-  }
 }
